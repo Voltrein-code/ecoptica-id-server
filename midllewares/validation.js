9b456dc193174e03795c30d51bcf85133464d00d
Voltrein-code <mr-voltrein@yandex.ru>
2023-05-27 18:24:07 +0300

    feat: Добавить валидацию для логина и регистрации

--- midllewares/validation.js
+++ midllewares/validation.js
@@ -18,3 +18,18 @@ module.exports.routesWithAuthValidation = celebrate({
     authorization: Joi.string().required(),
   }).unknown(true),
 });
+
+module.exports.signUpValidation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(100).required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports.signInValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
