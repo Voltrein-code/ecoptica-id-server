94fa106aac0e6216e5c79febbaf25d80833c3463
Voltrein-code <mr-voltrein@yandex.com>
2023-05-31 13:39:56 +0300

    ref: Рефакторинг index.js роута

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const { login, createUser } = require('../controllers/users');
 const auth = require('../midllewares/auth');
 const errorRouter = require('./error');
 
-const { signInValidation, signUpValidation, routesWithAuthValidation } = require('../midllewares/validation');
+const {
+  signInValidation,
+  signUpValidation,
+  routesWithAuthValidation,
+} = require('../midllewares/validation');
 
 router.post('/signin', signInValidation, login);
 router.post('/signup', signUpValidation, createUser);
