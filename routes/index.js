e112cbc13fd0b2cf7c87ecb2ff277f2af1311a43
Voltrein-code <mr-voltrein@yandex.com>
2023-05-31 09:07:33 +0300

    feat: Добавить роутинг

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
-const { login } = require('../controllers/users');
+const { login, createUser } = require('../controllers/users');
 const auth = require('../midllewares/auth');
+const errorRouter = require('./error');
 
-const { signInValidation, signUpValidation,  }
+const { signInValidation, signUpValidation, routesWithAuthValidation } = require('../midllewares/validation');
 
 router.post('/signin', signInValidation, login);
+router.post('/signup', signUpValidation, createUser);
+
+router.use('/users', routesWithAuthValidation, auth, require('./users'));
+
+router.use('/', errorRouter);
