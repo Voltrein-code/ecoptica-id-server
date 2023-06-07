87555ea0919c82c439739529b2c550df62cc0324
Voltrein-code <mr-voltrein@yandex.ru>
2023-06-07 16:56:10 +0300

    feat: Добавить роут смены пароля

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 
 const {
-  getUsers, getCurrentUser, getUserById, updateUser,
+  getUsers, getCurrentUser, getUserById, updateUser, changePassword,
 } = require('../controllers/users');
-const { updateUserValidation, userIdValidation } = require('../midllewares/validation');
+const { updateUserValidation, userIdValidation, changePasswordValidation } = require('../midllewares/validation');
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 router.get('/:userId', userIdValidation, getUserById);
 router.patch('/me', updateUserValidation, updateUser);
+router.patch('/me/password', changePasswordValidation, changePassword);
 
 module.exports = router;
